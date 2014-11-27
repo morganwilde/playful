6818f064321f32efc062c49707c5a133294ac945
Morgan Wilde <wilde.morgan@gmail.com>
2014-11-27 02:39:03 +0200

    Added circle to main demo and 2 screens to the readme

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 Playful is a high level API for drawing. It uses OpenGL to create any sort of user interface.
 
+### Created Circle model
+![Circle with 100 segments](https://dl.dropboxusercontent.com/u/31042440/playful/screen-circl-100-segments.png)
+![Circle with 10 segments](https://dl.dropboxusercontent.com/u/31042440/playful/screen-circle-10-segments.png)
+
 ### Added color to ShapesArray
 ![ShapesArray with color](https://dl.dropboxusercontent.com/u/31042440/playful/screen-shapes-array-color.png)
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 // Models
 #include "Models/ShapeTriangle.h"
 #include "Models/Rectangle.h"
+#include "Models/Circle.h"
 
 int main(int argc, char *argv[])
 {
@@ -15,17 +16,20 @@ int main(int argc, char *argv[])
     ShapeTriangle *triangle2 = new ShapeTriangle(Point(100, 200), Point(400, 200), Point(200, 500));
     Rectangle *rect1 = new Rectangle(Point(0, 0, 0), 200, 200);
     Rectangle *line = new Rectangle(Point(400, 0, 0), 20, 600);
+    Circle *circle = new Circle(Point(400, 300), 200, 100);
 
     // Add color
     triangle1->color = Color(1, 0, 0, 1);
     triangle2->color = Color(0.5, 0, 0, 1);
     rect1->setColor(Color(0, 1, 0, 1));
     line->setColor(Color(0, 0, 1, 1));
+    circle->setColor(Color(1, 1, 0, 1));
 
     window.shapesArray.add(triangle1);
     window.shapesArray.add(triangle2);
     window.shapesArray.add(rect1);
     window.shapesArray.add(line);
+    window.shapesArray.add(circle);
 
     window.glutDisplayLoop();
 
