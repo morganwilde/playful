1c710827436fc22dea87e9649cd514a67cb1dcc1
Morgan Wilde <wilde.morgan@gmail.com>
2015-01-26 00:48:59 +0200

    Completed area alignment

--- Controllers/CanvasController.cpp
+++ Controllers/CanvasController.cpp
@@ -20,19 +20,20 @@ void CanvasController::show()
 
 void CanvasController::setup()
 {
-    // Proportions for screen areas
+    // Padding is between each one of the two areas and the window
     double padding = 10;
+    // Proportions for screen areas
     double canvasWidthRelative = 0.8;
     double toolsWidthRelative = 1 - canvasWidthRelative;
 
-    double canvasWidth = window.getWidth()*canvasWidthRelative;
-    double canvasHeight = window.getHeight();
-    double toolsWidth = window.getWidth()*toolsWidthRelative;
-    double toolsHeight = window.getHeight();
+    double canvasWidth = window.getWidth()*canvasWidthRelative - 2*padding + padding/2;
+    double canvasHeight = window.getHeight() - 2*padding;
+    double toolsWidth = window.getWidth()*toolsWidthRelative - 2*padding + padding/2;
+    double toolsHeight = window.getHeight() - 2*padding;
 
     // Create area objects
-    Rectangle *canvasArea = new Rectangle(Point(0, 0, 0), canvasWidth, canvasHeight);
-    Rectangle *toolsArea = new Rectangle(Point(canvasWidth, 0, 0), toolsWidth, toolsHeight);
+    Rectangle *canvasArea = new Rectangle(Point(padding, padding, 0), canvasWidth, canvasHeight);
+    Rectangle *toolsArea = new Rectangle(Point(canvasWidth + 2*padding, padding, 0), toolsWidth, toolsHeight);
 
     // Color them
     canvasArea->setColor(Color(1, 0, 0, 1));
@@ -42,6 +43,7 @@ void CanvasController::setup()
     appendShape(canvasArea);
     appendShape(toolsArea);
 }
+
 void CanvasController::appendShape(ShapesArray *shape)
 {
     window.shapesArray.add(shape);
